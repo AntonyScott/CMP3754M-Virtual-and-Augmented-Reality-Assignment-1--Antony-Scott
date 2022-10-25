7aafdd7f3f012089ae6e810c7151b1e460d1dc7a
AntonyScott <antonyscott1@hotmail.co.uk>
2022-10-25 20:43:45 +0100

    Commit 4
    Completed workshop 5 and imported HD low poly car models.

---
+++ Assets/Scripts/TrafficLights.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrafficLights : MonoBehaviour
+{
+    public Transform t1;
+    public Transform t2;
+    public Transform t3;
+
+    public GameObject t1green;
+    public GameObject t1red;
+
+    public GameObject t2green;
+    public GameObject t2red;
+
+    public GameObject t3green;
+    public GameObject t3red;
+
+    public float stateTimer;
+    public int state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        t1 = transform.Find("TL1");
+        t2 = transform.Find("TL2");
+        t3 = transform.Find("TL3");
+
+        t1green = t1.Find("Green light").gameObject;
+        t1red = t1.Find("Red light").gameObject;
+        t2green = t2.Find("Green light").gameObject;
+        t2red = t2.Find("Red light").gameObject;
+        t3green = t3.Find("Green light").gameObject;
+        t3red = t3.Find("Red light").gameObject;
+
+        stateTimer = 10.0f;
+        SetState(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        stateTimer = stateTimer -= Time.deltaTime;
+        if(stateTimer <= 5)
+        {
+            SetState(2);
+            stateTimer = 10.0f;
+        }
+    }
+
+    void SetState(int c)
+    {
+        state = c;
+
+        if(c == 1)
+        {
+            //green, green, red
+            t1green.SetActive(true);
+            t1red.SetActive(false);
+            t2green.SetActive(false);
+            t2red.SetActive(true);
+            t3green.SetActive(false);
+            t3red.SetActive(true);
+        }
+        if(c == 2)
+        {
+            //red, red, green
+            t1green.SetActive(false);
+            t1red.SetActive(true);
+            t2green.SetActive(false);
+            t2red.SetActive(true);
+            t3green.SetActive(true);
+            t3red.SetActive(false);
+        }
+        else
+        {
+            //red, green, green
+            t1green.SetActive(false);
+            t1red.SetActive(true);
+            t2green.SetActive(true);
+            t2red.SetActive(false);
+            t3green.SetActive(true);
+            t3red.SetActive(false);
+        }
+    }
+}
